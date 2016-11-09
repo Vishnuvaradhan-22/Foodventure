5c7ba1034715fcbc6df4abf200a1085c9e67eb9e
Vish <svaradhan92@gmail.com>
2016-11-10 06:45:21 +1100

    Added Firebase Login.

--- FoodVenture/app/src/main/java/com/vish/foodventure/LaunchScreenActivity.java
+++ FoodVenture/app/src/main/java/com/vish/foodventure/LaunchScreenActivity.java
@@ -1,10 +1,31 @@
 package com.vish.foodventure;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.Serializable;
 
 public class LaunchScreenActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,7 +33,116 @@ public class LaunchScreenActivity extends AppCompatActivity {
         initializeUi();
     }
 
-    private void initializeUi(){
-
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    private void initializeUi(){
+        Button loginButton = (Button)findViewById(R.id.loginButton);
+        Button createAccount = (Button)findViewById(R.id.createAccount);
+
+        loginButton.setOnClickListener(loginListener);
+        createAccount.setOnClickListener(createAccountListener);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent();
+                    Bundle bundle = new Bundle();
+                    bundle.putString("UserEmail",user.getEmail());
+                    intent.setClass(getApplicationContext(),HomeScreen.class);
+                    startActivity(intent);
+                }
+            }
+        };
+    }
+
+    private View.OnClickListener loginListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            loginUser();
+        }
+    };
+
+    private View.OnClickListener createAccountListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            createNewAccount();
+        }
+    };
+
+
+    private void loginUser(){
+        EditText emailId = (EditText)findViewById(R.id.emailId);
+        final EditText password = (EditText)findViewById(R.id.password);
+
+        progressDialog = ProgressDialog.show(this, "FoodVenture",
+                "Login", true);
+
+
+        mAuth.signInWithEmailAndPassword(emailId.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (!task.isSuccessful()) {
+                            Exception exception = task.getException();
+
+                            switch (exception.getClass().getSimpleName()){
+                                case "FirebaseAuthInvalidUserException":
+                                    Toast.makeText(LaunchScreenActivity.this,"Please register with your email",Toast.LENGTH_LONG).show();
+                                    break;
+                                case "FirebaseAuthInvalidCredentialsException":
+                                    password.setError("Incorrect password!");
+                                    break;
+                            }
+                        }
+
+                        progressDialog.dismiss();
+
+                    }
+                });
+    }
+
+    private void createNewAccount(){
+        EditText emailId = (EditText)findViewById(R.id.emailId);
+        EditText password = (EditText)findViewById(R.id.password);
+
+        mAuth.createUserWithEmailAndPassword(emailId.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("My APp:", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Exception exception = task.getException();
+                            if(exception.getClass().getSimpleName().equals("FirebaseAuthUserCollisionException")){
+                                Toast.makeText(LaunchScreenActivity.this, "User already registered! Please Signin",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        else{
+
+                        }
+
+                    }
+                });
+    }
+
 }
